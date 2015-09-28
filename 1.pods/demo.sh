c8fc6ad72127a4c2519a46ae057c388106147325
Tim Hockin <thockin@google.com>
2015-09-28 14:17:59 -0700

    Add simple pod demo

---
+++ 1.pods/demo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kubectl --namespace=demos get pods
+
+kubectl --namespace=demos create -f pod.yaml
+
+kubectl --namespace=demos get pods
+
+kubectl --namespace=demos get pod pods-demo-pod -o yaml | grep podIP
+
+IP=$(kubectl --namespace=demos get pod pods-demo-pod -o yaml \
+        | grep podIP \
+        | cut -f2 -d:)
+NODE=$(kubectl get nodes \
+        | tail -1 \
+        | cut -f1 -d' ')
+gcloud compute ssh --zone=us-central1-b $NODE \
+    --command "while true; do curl -s $IP; sleep 0.5; done"
