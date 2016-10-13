3833d232db167638ddb81d9b2199d4bdc404b3d9
Christian Posta <christian.posta@gmail.com>
2016-10-13 16:39:48 -0700

    tidy up a bit

--- bluegreen/demo.sh
+++ bluegreen/demo.sh
@@ -5,6 +5,10 @@
 desc "Create the blue/v1 service"
 run "cat $(relative app-blue-v1.yaml)"
 run "kubectl --namespace=demos create -f $(relative app-blue-v1.yaml)"
+run "kubectl --namespace=demos get pod"
+run "kubectl --namespace=demos get svc"
+
+desc "expose our service to the outside world"
 run "oc expose svc/app-blue-v1 --name bluegreen"
 run "oc get route"
 
@@ -15,8 +19,8 @@ read -s
 desc "Run green/v2 of our app"
 run "cat $(relative app-green-v2.yaml)"
 run "kubectl --namespace=demos create -f $(relative app-green-v2.yaml)"
-run "kubectl --namespace=demos get svc"
 run "kubectl --namespace=demos get pod"
+run "kubectl --namespace=demos get svc"
 
 desc "Smoke test v2 in another window"
 read -s
@@ -25,4 +29,5 @@ desc "Patch the route to switch from blue to green"
 run "oc patch route/bluegreen -p '{\"spec\": {\"to\": {\"name\": \"app-green-v2\" }}}'"
 
 desc "Does everything look okay? Oh, no? fail back!"
+read -s
 run "oc patch route/bluegreen -p '{\"spec\": {\"to\": {\"name\": \"app-blue-v1\" }}}'"
