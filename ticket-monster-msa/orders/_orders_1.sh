a2ca835cffa74566dca0fa499400906430933818
Christian Posta <christian.posta@gmail.com>
2016-10-16 08:45:04 -0700

    cleaner curl

--- ticket-monster-msa/orders/_orders_1.sh
+++ ticket-monster-msa/orders/_orders_1.sh
@@ -2,9 +2,11 @@
 
 . $(dirname ${BASH_SOURCE})/../../util.sh
 
-desc "Let local service bootstrap"
-read -s
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/health); do
+    printf 'trying...\n'
+    sleep 5
+done
 
 desc "Add a booking"
-run "curl -s -v -X POST -H \"Accept: application/json\" -H \"Content-Type: application/json\" -d '{\"ticketRequests\":[{\"ticketPriceGuideId\":1,\"quantity\":1}],\"email\":\"foo@bar.com\",\"performance\":1,\"performanceName\":\"Rock concert of the decade at Roy Thomson Hall\"}'  http://localhost:8080/orders/bookings | pretty-json"
+run "curl -s -X POST -H \"Accept: application/json\" -H \"Content-Type: application/json\" -d '{\"ticketRequests\":[{\"ticketPriceGuideId\":1,\"quantity\":1}],\"email\":\"foo@bar.com\",\"performance\":1,\"performanceName\":\"Rock concert of the decade at Roy Thomson Hall\"}'  http://localhost:8080/orders/bookings | pretty-json"
 
