251e79b37cfd8b7f4b95b92b20620144460a0ba4
Christian Posta <christian.posta@gmail.com>
2016-10-17 15:11:24 -0700

    added tmux to bluegreen

--- bluegreen/demo.sh
+++ bluegreen/demo.sh
@@ -2,6 +2,8 @@
 
 . $(dirname ${BASH_SOURCE})/../util.sh
 
+SOURCE_DIR=$PWD
+
 desc "Create the blue/v1 service"
 run "cat $(relative app-blue-v1.yaml)"
 run "kubectl --namespace=demos create -f $(relative app-blue-v1.yaml)"
@@ -13,7 +15,10 @@ run "oc expose svc/app-blue-v1 --name bluegreen"
 run "oc get route"
 
 
-desc "Start load in tmux, another window"
+tmux split-window -v -d -c $SOURCE_DIR
+tmux send-keys -t bottom C-z './_bg_1.sh' Enter
+
+desc "run some load.."
 read -s
 
 desc "Run green/v2 of our app"
@@ -22,14 +27,17 @@ run "kubectl --namespace=demos create -f $(relative app-green-v2.yaml)"
 run "kubectl --namespace=demos get pod"
 run "kubectl --namespace=demos get svc"
 
-desc "Smoke test v2 in another window"
-read -s
+tmux split-window -h -d -c $SOURCE_DIR
+tmux send-keys -t right C-z './_bg_2.sh' Enter
 
 desc "Everything looks good! Let's upgrade!"
 read -s
 
 desc "Patch the route to switch from blue to green"
 run "oc patch route/bluegreen -p '{\"spec\": {\"to\": {\"name\": \"app-green-v2\" }}}'"
+
+tmux send-keys -t right C-c
+tmux send-keys -t right C-z 'exit' Enter
 
 desc "Does everything look okay? Oh, no? fail back!"
 read -s
@@ -43,6 +51,9 @@ desc "Note... Blue/Green is NOT the same as A/B, but we'll illustrated weighted 
 read -s
 
 run "oc patch route/bluegreen -p '{\"spec\": { \"alternateBackends\" : [{ \"kind\": \"Service\", \"name\": \"app-green-v2\", \"weight\": 25}], \"to\": {\"weight\": 75}}}'"
+
+tmux send-keys -t bottom C-c
+tmux send-keys -t bottom C-z 'exit' Enter
 
 
 
