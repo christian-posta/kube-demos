1276dc37652ac7a75f69e49238636dcfaf98ebb9
Christian Posta <christian.posta@gmail.com>
2016-10-17 15:17:56 -0700

    added tmux panes back to rolling update

--- rolling_update/_rolling_2.sh
+++ rolling_update/_rolling_2.sh
@@ -2,7 +2,4 @@
 
 . $(dirname ${BASH_SOURCE})/../util.sh
 
-desc "Do a rolling update to v2"
-run "cat $(relative rc-v2.yaml)"
-run "kubectl --namespace=demos rolling-update \\
-    update-demo-rc-v1 -f $(relative rc-v2.yaml) --update-period=5s"
+
--- rolling_update/demo.sh
+++ rolling_update/demo.sh
@@ -2,6 +2,9 @@
 
 . $(dirname ${BASH_SOURCE})/../util.sh
 
+SOURCE_DIR=$PWD
+
+
 desc "Create a service that fronts any version of this demo"
 run "cat $(relative svc.yaml)"
 run "kubectl --namespace=demos create -f $(relative svc.yaml)"
@@ -11,9 +14,17 @@ run "cat $(relative rc-v1.yaml)"
 run "kubectl --namespace=demos create -f $(relative rc-v1.yaml)"
 run "kubectl --namespace=demos get pods -l demo=update"
 
+tmux split-window -v -d -c $SOURCE_DIR
+tmux send-keys -t bottom C-z './_rolling_1.sh' Enter
 
-echo "Manually show the rolling-upgrade part by using tmux"
-#tmux new -d -s my-session \
-#    "$(dirname ${BASH_SOURCE})/_rolling_1.sh" \; \
-#    split-window -h -d "sleep 15; $(dirname $BASH_SOURCE)/_rolling_2.sh" \; \
-#    attach \;
+desc "Prep the load"
+read -s
+
+desc "Do a rolling update to v2"
+run "cat $(relative rc-v2.yaml)"
+run "kubectl --namespace=demos rolling-update \\
+    update-demo-rc-v1 -f $(relative rc-v2.yaml) --update-period=5s"
+
+tmux send-keys -t bottom C-c
+tmux send-keys -t bottom C-z 'exit' Enter
+
