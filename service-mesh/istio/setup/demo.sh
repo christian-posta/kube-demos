af93fd014c1248d56fe734e424289ad202494b8e
Christian Posta <christian.posta@gmail.com>
2017-05-11 13:59:18 -0700

    fixed issues with recent master changes on istio

--- service-mesh/istio/setup/demo.sh
+++ service-mesh/istio/setup/demo.sh
@@ -16,12 +16,6 @@ if [ "$1" == "--upstream" ]; then
         git clone https://github.com/istio/istio.git $(relative project/istio)
     fi
 
-    if [ "$2" == "--zipkin" ]; then
-        pushd $(relative project/istio)
-        curl -s -L https://gist.github.com/christian-posta/d6192ada85ed65a8a99047e38f2779e0/raw | git apply -
-        popd
-    fi
-
     ADDONS=$(relative project/istio/install/kubernetes/addons)
     INSTALL=$(relative project/istio/install/kubernetes/istio.yaml)
 
@@ -29,6 +23,10 @@ if [ "$1" == "--upstream" ]; then
     source $(relative project/istio/istio.VERSION)
     curl -s ${ISTIOCTL_URL}/istioctl-osx > $(relative cli/istioctl)
     chmod +x $(relative cli/istioctl)
+fi
+
+if [ "$2" == "--zipkin" ]; then
+    INSTALL=$(relative zipkin/istio.yaml)
 fi
 
 echo "Using $INSTALL for the installation"
@@ -42,10 +40,6 @@ read -s
 
 echo "Let's install the istio addons"
 kubectl create -f $ADDONS
-
-if [ "$2" = "--zipkin" ]; then
-    curl -s -L https://gist.github.com/christian-posta/c5f61d68913c35cdb3ea005e17a8adbd/raw | kubectl create -f -
-fi
 
 echo "Let's install the istio ingress controller, mixer, and manager"
 kubectl create -f $INSTALL
