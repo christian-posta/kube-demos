736a4da9e2e6faf791819ec8d225cb6f24b21102
Christian Posta <christian.posta@gmail.com>
2017-06-20 13:54:05 -0700

    changed setup script to not stdout

--- setup.sh
+++ setup.sh
@@ -2,21 +2,10 @@
 
 . $(dirname ${BASH_SOURCE})/util.sh
 
-desc "The demo namespace does not exist"
-run "oc new-project demos"
 
-desc "For openshift, we need to enable deployer rights"
-run "oc policy add-role-to-user edit system:serviceaccount:demos:deployer"
+oc new-project demos
+oc policy add-role-to-user edit system:serviceaccount:demos:deployer
 
-desc "Also enable default service account to talk to Kube API"
-run "oc policy add-role-to-user edit system:serviceaccount:demos:default"
-run "oc adm policy add-scc-to-user anyuid system:serviceaccount:demos:default"
-
-# enable this so that apps can see configmaps
-run "oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)"
-
-desc "Hey look, a namespace!"
-run "kubectl get namespaces"
-
-
-
+oc policy add-role-to-user edit system:serviceaccount:demos:default
+oc adm policy add-scc-to-user anyuid system:serviceaccount:demos:default
+oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
