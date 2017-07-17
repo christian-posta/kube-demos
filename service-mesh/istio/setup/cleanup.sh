e1bc9236eee1d6475bd847a869e6356845ad368c
Christian Posta <christian.posta@gmail.com>
2017-07-17 06:52:22 -0700

    updated to use tags of istio

--- service-mesh/istio/setup/cleanup.sh
+++ service-mesh/istio/setup/cleanup.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 . $(dirname ${BASH_SOURCE})/../../../util.sh
 
-rm $(relative cli/istioctl)
 
 # delete addons
 kubectl delete deploy/grafana
 kubectl delete deploy/prometheus
 kubectl delete deploy/servicegraph
+kubectl delete deploy/zipkin
 
 kubectl delete svc/grafana
 kubectl delete svc/prometheus
 kubectl delete svc/servicegraph
+kubectl delete svc/zipkin
 
 kubectl delete cm/prometheus
 
 # delete istio infra
-kubectl delete deploy/istio-ingress-controller
-kubectl delete deploy/istio-manager
-kubectl delete deploy/istio-mixer
-
-kubectl delete svc/istio-ingress-controller
-kubectl delete svc/istio-manager
-kubectl delete svc/istio-mixer
-
-kubectl delete cm/mixer-config
-kubectl delete cm/istio
-
-# some of the new things
-kubectl delete cm/prometheus
-kubectl delete svc/istio-egress
-kubectl delete svc/istio-ingress
-kubectl delete svc/zipkin
 kubectl delete deploy/istio-egress
 kubectl delete deploy/istio-ingress
-kubectl delete deploy/zipkin
+kubectl delete deploy/istio-pilot
+kubectl delete deploy/istio-mixer
+
+kubectl delete svc/istio-ingress
+kubectl delete svc/istio-egress
+kubectl delete svc/istio-pilot
+kubectl delete svc/istio-mixer
+
+kubectl delete cm/istio
+
 kubectl delete sa/istio-ingress-service-account 
-kubectl delete sa/istio-manager-service-account 
-kubectl delete deploy/servicegraph
+kubectl delete sa/istio-pilot-service-account 
--- service-mesh/istio/setup/demo.sh
+++ service-mesh/istio/setup/demo.sh
@@ -2,8 +2,9 @@
 
 . $(dirname ${BASH_SOURCE})/../../../util.sh
 
-ADDONS=$(relative addons)
-INSTALL=$(relative install)
+VERSION="0.1.6"
+ADDONS=$(relative binaries/istio-$VERSION/install/kubernetes/addons)
+INSTALL=$(relative binaries/istio-$VERSION/install/kubernetes/istio.yaml)
 
 if [ "$1" == "--upstream" ]; then
     echo "installing from upstream..."
@@ -18,13 +19,23 @@ if [ "$1" == "--upstream" ]; then
 
     ADDONS=$(relative project/istio/install/kubernetes/addons)
     INSTALL=$(relative project/istio/install/kubernetes/istio.yaml)
+    VERSION="master"
 
     #Download upstream client 
     source $(relative project/istio/istio.VERSION)
-    curl -s ${ISTIOCTL_URL}/istioctl-osx > $(relative cli/istioctl)
-    chmod +x $(relative cli/istioctl)
+    curl -s ${ISTIOCTL_URL}/istioctl-osx > $(relative project/bin/istioctl)
+    chmod +x $(relative project/bin/istioctl)
+else
+    if [ ! -d "$(relative binaries/istio-$VERSION)" ]; then
+        rm -fr $(relative binaries/istio-*)
+        pushd $(relative binaries)
+        curl -L -O "https://github.com/istio/istio/releases/download/$VERSION/istio-$VERSION-osx.tar.gz"
+        tar -xzf "istio-$VERSION-osx.tar.gz"
+        popd
+    fi 
 fi
 
+echo "Using version:  $VERSION"
 echo "Using $INSTALL for the installation"
 echo "Using $ADDONS for the addons"
 
@@ -37,3 +48,11 @@ kubectl create -f $ADDONS
 
 echo "Let's install the istio ingress controller, mixer, and manager"
 kubectl create -f $INSTALL
+
+
+#####
+# Install notes
+# download 0.1.6
+# https://github.com/istio/istio/releases/download/0.1.6/istio-0.1.6-osx.tar.gz
+# unpack the tar file
+# now the istio-0.1.6 has a folder structure with addons, install, and bin
